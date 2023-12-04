d4734f096909f5d9b82d3b005dcf8fd48df129ad
Nnaemeka Emesowum <nna.emesowum@gmail.com>
2023-12-04 10:34:44 +0100

    created server and installed packages and dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to File Uploader</h1>
+    </>
+  );
+}
+
+export default App;
